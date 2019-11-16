aabfadfb30713423d0449e6f065904f5fe068282
johnsaleena <sj674@njit.edu>
2019-11-16 02:01:47 -0500

    Feature: P_value.py file created

---
+++ Statistics/P_value.py
@@ -0,0 +1,11 @@
+from Statistics.Zscore import zscore
+
+
+def P_value(data):
+    data = []
+    n = len(data)
+    score = zscore()
+    for index in n:
+        if n == score:
+            data.append(index)
+    return index
