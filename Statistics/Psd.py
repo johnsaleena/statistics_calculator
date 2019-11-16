95eadbed2324cae5e779dc1ed42064f7cc525829
johnsaleena <sj674@njit.edu>
2019-11-16 02:02:47 -0500

    Feature: Population standard deviation file created

---
+++ Statistics/Psd.py
@@ -0,0 +1,18 @@
+from Calculator.Addition import addition
+from Calculator.Division import division
+from Calculator.Square import squaree
+from Calculator.Square_rot import squar_rot
+from Calculator.Subtraction import subtraction
+from Statistics.Mean import mean
+
+
+def psd(numbers):
+    num_values = len(numbers)
+
+    result = mean(numbers)
+    total = 0
+    for numb in numbers:
+        result2 = subtraction(numb, result)
+        sq = squaree(result2)
+        total = addition(total, sq)
+    return squar_rot(division(num_values, total))
